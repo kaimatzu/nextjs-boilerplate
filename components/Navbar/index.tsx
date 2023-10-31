d6b6c23a92e8b4ac24f4de1210ff896cfa61386f
kaimatzu <karljohnvillardar@gmail.com>
2023-10-31 18:17:36 +0800

    v1

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,12 +1,31 @@
+"use client";
 import React from "react";
 import style from "./style.module.css";
 import { Button } from "@/components";
+import { useState } from "react";
 
 function Navbar() {
+  const [wallet, setwallet] = useState<null | string>(null);
+
+  const connectWallet = async () => {
+    try{
+      const { solana } = window as any;
+      const response = await solana.connect();
+      setwallet(response.publicKey.toString());
+    }
+    catch (_){
+      alert(" You have no wallet in your browser");
+    }
+  };
+
+  const parseWalletKey = (string: string) => {
+    return `${string.slice(0, 5)}......${string.slice(-5)}`;
+  };
+
   return (
     <>
       <div className={style.toolbar}>
-        <h4>Logo</h4>
+        <h4>XD</h4>
 
         <div className={style.links_container}>
           <p>Marketplace</p>
@@ -15,8 +34,9 @@ function Navbar() {
         </div>
 
         <div className={style.links_container}>
-          <Button outlined>Sign In</Button>
-          <Button>Get Started</Button>
+          <Button onClick={connectWallet}>
+            {wallet ? parseWalletKey(wallet) : "Connect Wallet"}
+          </Button>
         </div>
       </div>
       <div className={style.divider}></div>
